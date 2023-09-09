00442838a698672d007f18409817f51e5d6057c0
Diana Salazar <desalazarguerrero@gmail.com>
2023-09-08 21:09:43 -0700

    block07 assignment-secure the vault

---
+++ block07/index.js
@@ -0,0 +1,20 @@
+//create first variable using arithmetic operator equal to 10
+
+let a = 30 / 3;
+
+//create second variable using arithmetic operator equal to 40
+
+let b = 17 + 23;
+
+//create first variable using arithmetic operator equal to 39
+
+let c = 40 - 1;
+
+//create string with message to vault opener
+let greeting ="You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+
+//const userMessage = 'You have received this message because you have been chosen to open an important vault. Here is the secret combination: ${a} ${b} ${c}'
+//const userMessage='hello ${a}'
+
+''console.log({userMessage})
+alert(greeting a b c);
